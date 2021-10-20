65ffc9a9844e26cc2cade792c6185a9f8c12a9d6
vhh <vhh0106@gmail.com>
2021-10-20 17:07:24 +0700

    Them MenuAdminGUI

---
+++ src/UTILS/md5.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTILS;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author vhh01
+ */
+public class md5 {
+    public static String md5(String msg) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(msg.getBytes());
+            byte byteData[] = md.digest();
+            //convert the byte to hex format method 1
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return  sb.toString();
+        } catch (Exception ex) {
+            return "";
+        }
+    }    
+}
